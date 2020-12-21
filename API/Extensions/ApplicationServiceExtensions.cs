28d71853c6a3118bd5c1b6e9db79becaf01da052
Demetrix98 <pawlowicz.k@wp.pl>
2020-12-21 17:58:50 +0100

    Adding photo upload functionality

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -1,3 +1,4 @@
+  
 using API.Data;
 using API.Helpers;
 using API.Interfaces;
@@ -13,8 +14,10 @@ namespace API.Extensions
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
         {
-            services.AddScoped<IUserRepository, UserRepository>();
+            services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
             services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IPhotoService, PhotoService>();
+            services.AddScoped<IUserRepository, UserRepository>();
             services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
             services.AddDbContext<DataContext>(options =>
             {
