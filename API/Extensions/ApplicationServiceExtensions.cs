38ae07938e6e937736e1e1663e2b7d8c35653475
Demetrix98 <pawlowicz.k@wp.pl>
2020-12-22 15:32:10 +0100

    Paging, Sorting and Filtering

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -1,4 +1,3 @@
-  
 using API.Data;
 using API.Helpers;
 using API.Interfaces;
@@ -17,6 +16,7 @@ namespace API.Extensions
             services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
             services.AddScoped<ITokenService, TokenService>();
             services.AddScoped<IPhotoService, PhotoService>();
+            services.AddScoped<LogUserActivity>();
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
             services.AddDbContext<DataContext>(options =>
