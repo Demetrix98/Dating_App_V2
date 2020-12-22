7056dbeff8724d0172563563c6bd7c66c5595043
Demetrix98 <pawlowicz.k@wp.pl>
2020-12-22 18:54:30 +0100

    Adding the messaging feature

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -6,7 +6,7 @@ namespace API.Entities
 {
     public class AppUser
     {
-            public int Id { get; set; }
+        public int Id { get; set; }
         public string UserName { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
@@ -24,5 +24,8 @@ namespace API.Entities
 
         public ICollection<UserLike> LikedByUsers { get; set; }
         public ICollection<UserLike> LikedUsers { get; set; }
+
+        public ICollection<Message> MessagesSent { get; set; }
+        public ICollection<Message> MessagesReceived { get; set; }
     }
 }
