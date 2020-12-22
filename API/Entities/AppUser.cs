fddc8d1061ff96c525ab37ee8f11b4f1a0e2941e
Demetrix98 <pawlowicz.k@wp.pl>
2020-12-22 16:52:28 +0100

    Adding the Like User feature

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -6,7 +6,7 @@ namespace API.Entities
 {
     public class AppUser
     {
-        public int Id { get; set; }
+            public int Id { get; set; }
         public string UserName { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
@@ -22,10 +22,7 @@ namespace API.Entities
         public string Country { get; set; }
         public ICollection<Photo> Photos { get; set; }
 
-        // public int GetAge()
-        // {
-        //     return DateOfBirth.CalculateAge();
-        // }
-
+        public ICollection<UserLike> LikedByUsers { get; set; }
+        public ICollection<UserLike> LikedUsers { get; set; }
     }
 }
